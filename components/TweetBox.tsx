c598966408b33df7a9af65a313dd62d5befc08d9
Octavio De Salvatore <octaviodesalvatore@MacBook-Pro-de-Octavio.local>
2022-05-27 16:19:40 -0300

    responsive

---
+++ components/TweetBox.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TweetBox() {
+  return (
+    <div>
+      <img
+        className="mt-4 h-14 w-14 rounded-full object-cover"
+        src="https://links.papareact.com/gll"
+        alt="user picture"
+      />
+      <div>
+        <form></form>
+      </div>
+    </div>
+  )
+}
+
+export default TweetBox
