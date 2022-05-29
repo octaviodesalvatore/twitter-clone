6209136167ca08760f73f2bdee1b6ab92827507c
Octavio De Salvatore <octaviodesalvatore@MacBook-Pro-de-Octavio.local>
2022-05-28 22:54:03 -0300

    comments update

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Tweet } from '../typings'
+import React, { useState, useEffect } from 'react'
+import { Comment, Tweet } from '../typings'
 import Timeago from 'react-timeago'
+import { fetchComments } from '../utils/fetchComments'
 
 import {
   ChatAlt2Icon,
@@ -14,6 +15,16 @@ interface Props {
 }
 
 function Tweet({ tweet }: Props) {
+  const [comments, setComments] = useState<Comment[]>([])
+  const refreshComments = async () => {
+    const comments: Comment[] = await fetchComments(tweet._id)
+    setComments(comments)
+  }
+
+  useEffect(() => {
+    refreshComments()
+  }, [])
+
   return (
     <div className="space-x3 flex flex-col border-y border-gray-100 p-5">
       <div className="flex space-x-3">
@@ -27,7 +38,7 @@ function Tweet({ tweet }: Props) {
           <div className="flex items-center space-x-1">
             <p className="mr-1 font-bold">{tweet.username}</p>
             <p className="text-gray hidden text-sm sm:inline">
-              @{tweet.username.replace(/\s+/g, '').toLowerCase} ·
+              @nofunciona{tweet.username.replace(/\s+/g, '').toLowerCase} ·
             </p>
 
             <Timeago
@@ -48,7 +59,7 @@ function Tweet({ tweet }: Props) {
       <div className="mt-5 flex justify-between">
         <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <ChatAlt2Icon className="h-5 w-5" />
-          <p>5</p>
+          <p>{comments.length}</p>
         </div>
 
         <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
@@ -62,6 +73,35 @@ function Tweet({ tweet }: Props) {
           <UploadIcon className="h-5 w-5" />
         </div>
       </div>
+
+      {comments?.length > 0 && (
+        <div className="border-top my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-gray-100 p-5">
+          {comments.map((comment) => (
+            <div key={comment._id} className="relative flex space-x-2">
+              <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+              <img
+                src={comment.profileImg}
+                alt="Profile Image"
+                className="mt-2 h-7 w-7 rounded-full object-cover"
+              />
+              <div>
+                <div className="flex items-center space-x-1">
+                  <p className="mr-1 font-bold">{comment.username}</p>
+                  <p className="hidden text-sm text-gray-500 lg:inline">
+                    @nofunciona
+                    {comment.username.replace(/\s+/g, '').toLowerCase} ·
+                  </p>
+                  <Timeago
+                    className="text-sm text-gray-500"
+                    date={comment._createdAt}
+                  />
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
